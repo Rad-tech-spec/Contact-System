518e25715d3155dfb92d93e679819b6d8d1c7d9f
Rad Eshghi <64996038+Rad-tech-spec@users.noreply.github.com>
2021-05-04 16:31:49 -0700

    Update contacts.h

--- contacts.h
+++ contacts.h
@@ -1,30 +1,8 @@
-// ----------------------------------------------------------------------------
-// DO NOT remove the below 2 lines.
-// The below two lines is needed to ensure only one copy
-// of this header file is included when compiling the project.
-// (it is referred to as "safeguarding")
-// You will learn about this in C++ (OOP244/BTP200)
-// For now, simply leave this code in!
 #ifndef CONTACTS_H_
 #define CONTACTS_H_
-// ----------------------------------------------------------------------------
-
-//==============================================
-// Name:             
-// Student Number: 
-// Email:          @myseneca.ca
-// Section:        NBB
-// Date:           
-//==============================================
-// Assignment:     2
-// Milestone:      4
-//==============================================
-
-
 //--------------------------------
 // Structure Types
 //--------------------------------
-
 // +-------------------------------------------------+
 // | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
 // |        structures below:                        |
